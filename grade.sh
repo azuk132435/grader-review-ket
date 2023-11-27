11070e4d8ae09781cf1e75b2873afe07aef6bd6d
azuk132435 <146765369+azuk132435@users.noreply.github.com>
2023-11-27 10:58:49 -0800

    Update grade.sh
    update

--- grade.sh
+++ grade.sh
@@ -8,9 +8,31 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+Expected_file = "ListExamples.java"
+if [[ ! -e "student-submission/$Expected_file"]];
+then echo "Error"
+exit 1
+fi
+
+cp "student-submission/$Expected_file" grading-area/
+cd grading-area
+javac -cp "$CPATH" "Expected_file" TestListExamples.java
+
+if [[ $? -ne 0]]; then 
+echo "Error"
+exit 1
+fi
+
+java -cp "$CPATH" org.junit.runner.JunitCore testlistexamples > test_output.txt 2>&1
+if [[ $? -eq 0]]; then
+    grade=$(grep-oP "your grade is \k\d+" test_output.txt)
+    echo "Grade: $grade/100"
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
